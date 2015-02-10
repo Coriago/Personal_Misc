7cbeadba943df44df33e5b817845c897bb6024e1
Coriago <gagemiller155@gmail.com>
2015-02-10 10:02:42 -0500

    Added new compound file

---
+++ Compund_Intrest.py
@@ -0,0 +1,6 @@
+int(startMoney) = input("Enter starting money: ")
+int(rate) = input("Enter rate: ")
+int(time) = input("Enter interval time: ")
+int(compundAmt) = input("How many times to compund: ")
+result
+

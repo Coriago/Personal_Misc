692e4263315fd2377f4db2742109f6189e147bbb
TomAwezome <sonidd1@yahoo.com>
2015-02-22 14:40:58 -0500

    euler 11

--- largegridprod.py
+++ largegridprod.py
@@ -1,4 +1,4 @@
-#i'm really fucking eager to use a dictionary that actually works XD
+#i'm really eager to use a dictionary that actually works XD
 #i don't think we really can here :(
 gridstring="""08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
